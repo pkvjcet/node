1be5c0ce2ad5df1aaec33cb2ec1e32d993f3e034
renjitasony <renjitasony@gmail.com>
2019-10-05 13:46:00 +0530

    argument passing in events

--- Node Samples/app3.js
+++ Node Samples/app3.js
@@ -2,10 +2,10 @@
 const EventEmitter=require('events');
 var emitter=new EventEmitter; //object created
 //function definintion
-function myEventFunction(){
-    console.log("event occured");
-}
+
 //event binding
-emitter.on("myEvent",myEventFunction);
+emitter.on("myEvent",(a,b)=>{
+    console.log("event arg1="+a+"arg2="+b);
+});
 //event firing
-emitter.emit('myEvent');
+emitter.emit('myEvent',1,"prince");

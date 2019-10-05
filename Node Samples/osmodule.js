2a8b62de5f30e53f2e9c263fb9d18cb19e174f08
renjitasony <renjitasony@gmail.com>
2019-10-05 12:00:55 +0530

    template string

--- Node Samples/osmodule.js
+++ Node Samples/osmodule.js
@@ -1,6 +1,7 @@
 const os=require('os');
 var totMem=os.totalmem();
 var freemem=os.freemem();
-console.log("total memory="+totMem);
-console.log("free memory="+freemem);
-console.log(os.networkInterfaces());
+//template string
+console.log(`total memory:${totMem}`);
+console.log(`free memory:${freemem}`);
+// console.log(os.networkInterfaces());

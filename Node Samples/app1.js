92250820512cf420a7cfc681bec5e37d0215a599
renjitasony <renjitasony@gmail.com>
2019-10-05 11:25:06 +0530

    more exports

---
+++ Node Samples/app1.js
@@ -0,0 +1,3 @@
+var logger=require('./logger1');
+logger.display("welcome pk");
+console.log(logger.link);
---
+++ Node Samples/logger1.js
@@ -0,0 +1,6 @@
+var url='http://ictkerala.org';
+function print(msg){
+    console.log(msg);
+}
+module.exports.display=print;
+module.exports.link=url;

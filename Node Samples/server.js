bfecda701e83ef6afb71482f213d578704e3f62f
renjitasony <renjitasony@gmail.com>
2019-10-05 15:18:06 +0530

    multiple page handling using http

--- Node Samples/server.js
+++ Node Samples/server.js
@@ -1,7 +1,28 @@
-var http=require('http');
+var http=require('http'),url=require('url');
 var server=http.createServer(function(req,res){
-    res.write("Hello World");
-    res.end();
-
+    var urlParts=url.parse(req.url);
+    console.log(urlParts);
+    switch(urlParts.pathname){
+        case "/":
+            homepage(req,res);
+            break;
+        case "/read":
+            read(req,res);
+            break;
+        case "/user/update":
+            update(req,res);
+            break;
+    }
 });
-server.listen(8080);
+server.listen(8080,function(){
+    console.log("listening 8080....");
+});
+function homepage(req,res){
+        res.end("home page");
+}
+function read(req,res){
+    res.end("Read page");
+}
+function  update(req,res){
+    res.end("update page");
+}
